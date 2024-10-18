17ca6ee41f5f35b2efcf2aa1ca0ff7a992d5dc34
Brent Jure <brentjure@gmail.com>
2024-10-19 00:15:10 +0300

    added sticky navigation

--- script.js
+++ script.js
@@ -57,3 +57,21 @@ function checkFlexGap() {
 checkFlexGap();
 
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
+
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".hero-section");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (!ent.isIntersecting) document.body.classList.add("sticky");
+    if (ent.isIntersecting) document.body.classList.remove("sticky");
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
